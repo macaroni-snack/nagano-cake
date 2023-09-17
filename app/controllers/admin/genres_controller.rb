646cb82e27ad9a2e4e5c1e756b19288e41905178
EC2 Default User <ec2-user@ip-172-31-34-247.ap-northeast-1.compute.internal>
2023-09-17 08:28:36 +0000

    add-action-controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,13 @@
 class Admin::GenresController < ApplicationController
   def index
   end
+  
+  def create
+  end
 
   def edit
+  end
+  
+  def update
   end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,14 @@
 class Public::OrdersController < ApplicationController
   def new
   end
+  
+  def confirm
+  end
 
   def complete
+  end
+  
+  def create
   end
 
   def index

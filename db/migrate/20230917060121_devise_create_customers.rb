ffa4f75936983ca91da15427a68badb468936e37
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-18 05:53:31 +0000

    Add model

--- db/migrate/20230917060121_devise_create_customers.rb
+++ db/migrate/20230917060121_devise_create_customers.rb
@@ -31,8 +31,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-
+      t.string :family_name, null: false
+      t.string :farst_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :farst_name_kana, null: false
+      t.string :telephone_number, null: false
+      t.string :post_code, null: false
+      t.string :address, null: false
+      t.boolean :is_deleted, null: false, default: false
       t.timestamps null: false
     end
 

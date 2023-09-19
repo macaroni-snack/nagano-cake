357ff2d509be6076250ec7201308d224a1e45357
YUU522 <kuromu2750@gmail.com>
2023-09-19 09:22:48 +0000

    商品の登録を追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -39,6 +39,6 @@ class Admin::ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_active,:genre_id)
   end
 end

1aa20b0920c3462956d6b93582084ec0a2f5a984
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-19 08:14:22 +0000

    [Add]管理者の顧客ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,20 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+      @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        redirect_to admin_customer_path(@customer)
+      else
+        render :edit
+      end
   end
   
-  def update
+  private
+  def customer_params
+    params.require(:admin).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :email, :is_deleted)
   end
 end

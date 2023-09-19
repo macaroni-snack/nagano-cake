04c31b09c2f64e82ebdde117a75881da7a06e542
fukai99 <141290272+fukai99@users.noreply.github.com>
2023-09-19 08:20:53 +0000

    ジャンル機能完

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,35 @@
 class Admin::GenresController < ApplicationController
+
   def index
-  end
-  
-  def create
+    #投稿
+    @genre = Genre.new
+    #一覧
+    @genres = Genre.all
+
   end
 
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to '/admin/genres'
+  end
+
+
   def edit
+    @genre = Genre.find(params[:id])
   end
-  
+
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
   end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end

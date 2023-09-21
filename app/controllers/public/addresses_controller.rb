9b4afc22674b3d70b79fbb1f816185fd55aa85b0
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-21 09:37:27 +0000

    [Add]配送先一覧/登録画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,24 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
   end
 
   def edit
   end
-  
-  def create
-  end
-  
+
   def update
   end
-  
+
   def destroy
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name, :customer_id)
   end
 end

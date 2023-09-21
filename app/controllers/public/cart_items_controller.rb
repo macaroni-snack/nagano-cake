d831cb4b589fef2c9e71c255356a175e2ddf4dc2
EC2 Default User <ec2-user@ip-172-31-34-247.ap-northeast-1.compute.internal>
2023-09-21 10:02:40 +0000

    トップページとアバウトページ完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,6 +4,23 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
+    @cart_item_check = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
+    if @cart_item_check
+      @cart_item_check.amount += params[:cart_item][:amount].to_i
+      @cart_item_check.save
+      flash[:success] = "カートに存在済のアイテムです"
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
+        flash[:success] = "カートに追加しました"
+        redirect_to cart_items_path
+      else
+        flash[:danger] = "予期せぬエラーが発生しました"
+        redirect_back(fallback_location: root_path)
+      end
+    end
   end
   
   def update
@@ -16,5 +33,10 @@ class Public::CartItemsController < ApplicationController
     CartItem.where(customer_id: current_customer.id).destroy_all
     flash[:success] = "カートの中を空にしました"
     redirect_back(fallback_location: root_path)
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end

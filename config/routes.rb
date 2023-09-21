39202d17063055ab78cab2eecab72fc96640e941
YUU522 <kuromu2750@gmail.com>
2023-09-21 10:55:08 +0000

    注文情報入力画面

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get 'about' => "homes#about"
+    post "orders/confirm" => "orders#confirm"
+    get "orders/complete" => "orders#complete"
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :update]
     get 'customers/confirm_withdrawal' => "customers#confirm_withdrawal"
@@ -17,11 +19,8 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :update, :create, :destroy]
     delete 'cart_items/destroy_all' => "cart_items#destroy_all"
     resources :orders, only: [:new, :create, :index, :show]
-    post "orders/confirm" => "orders#confirm"
-    get "orders/complete" => "orders#complete"
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
-
   namespace :admin do
     get "" => "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
--- db/migrate/20230918043541_create_orders.rb
+++ db/migrate/20230918043541_create_orders.rb
@@ -5,6 +5,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.string :post_code, null: false
       t.string :address, null: false
+      t.string :address_option, null: false
       t.integer :shipping_fee, null: false
       t.integer :payment_method, null: false, default: 0
       t.integer :total_payment, null: false

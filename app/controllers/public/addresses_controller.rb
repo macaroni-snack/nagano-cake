408af09c3f967e48454944055f433a1744c38206
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-22 06:07:16 +0000

    [Add]配送先登録・編集・削除のフラッシュエラーメッセージ

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,8 +6,15 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
-    @address.save
-    render :index
+    @address.customer_id = current_customer.id #会員IDがログインしているユーザーの場合に限定してる
+    if @address.save
+      flash[:notice] = "新しい配送先の登録が完了しました。"
+      redirect_to addresses_path(@address)
+    else
+      @addresses = Address.all
+      flash.now[:notice] = "新しい配送先の登録に失敗しました。"
+      render :index
+    end
   end
 
   def edit
@@ -16,11 +23,25 @@ class Public::AddressesController < ApplicationController
 
   def update
     @address = Address.find(params[:id])
-  	@address.update(address_params)
-  	redirect_to addresses_path
+  	if @address.update(address_params)
+  	  flash[:notice] = "配送先の変更内容を保存しました。"
+  	  redirect_to addresses_path
+  	 else
+  	   flash.now[:notice] = "新しい配送先の変更に失敗しました。"
+  	   render :edit
+  	 end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.customer_id = current_customer.id
+    if @address.destroy
+      flash[:notice] = "配送先の削除が完了しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:notice] = "配送先の削除に失敗しました。"
+      render :edit
+    end
   end
 
   private

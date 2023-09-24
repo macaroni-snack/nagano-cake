cf1ce8f2c9b83bff580f45641edac205ec944adc
EC2 Default User <ec2-user@ip-172-31-34-247.ap-northeast-1.compute.internal>
2023-09-24 10:00:53 +0000

    追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_customer!, except: [:top, :about], unless: :admin_signed_in?
+before_action :authenticate_admin!, if: :admin_namespace?
+
+  private
+
+  def admin_namespace?
+    controller_path.start_with?('admin/')
+  end
+  
+  def after_sign_in_path_for(resource)
+    customer_path(current_customer)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end

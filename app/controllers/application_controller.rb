91bc048a2fc36410d24f9c5efad7a00b6daa472c
YUU522 <kuromu2750@gmail.com>
2023-09-24 09:58:30 +0000

    注文情報履歴の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!, except: [:top, :about], unless: :admin_signed_in?
+  before_action :authenticate_admin!, if: :admin_namespace?
+  
+  def admin_namespace?
+    controller_path.start_with?('admin/')
+  end
 end
